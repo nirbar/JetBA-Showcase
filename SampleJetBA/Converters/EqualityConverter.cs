672c0c014b1dfe4fe31a6ae86fcfbba4aa00dbd5
Nir Bar <nir.bar@panel-sw.co.il>
2019-12-11 16:31:07 +0200

    EqualityConverter can serve to compare to parameter and/or to multi-values

--- SampleJetBA/Converters/EqualityConverter.cs
+++ SampleJetBA/Converters/EqualityConverter.cs
@@ -5,8 +5,18 @@ using System.Windows.Data;
 
 namespace SampleJetBA.Converters
 {
-    public class EqualityConverter : BaseConverter, IMultiValueConverter
+    public class EqualityConverter : BaseConverter, IMultiValueConverter, IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(new object[] { value }, targetType, parameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
             if ((values == null) || (values.Length == 0))
@@ -15,12 +25,21 @@ namespace SampleJetBA.Converters
             }
 
             object val0 = values[0];
+            if ((parameter != null) && !parameter.Equals(val0))
+            { 
+                return false;
+            }
+
             for (int i = 1; i < values.Length; ++i)
             {
                 object val1 = values[i];
 
                 // If both are null, we'll continue; If just one is null, we'll return false; If neither is null, then actual comparison will be made.
                 if ((val0?.Equals(val1) ?? val1?.Equals(val0)) == false)
+                {
+                    return false;
+                }
+                if ((parameter != null) && !parameter.Equals(val1))
                 {
                     return false;
                 }
