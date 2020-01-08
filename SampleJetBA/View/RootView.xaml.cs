35fe4b7a098904c4a3c98d5daad2cf48d06df3dc
Nir Bar <nir.bar@panel-sw.co.il>
2020-01-08 16:18:54 +0200

    Use TextWithButton for folder browse

--- SampleJetBA/View/RootView.xaml.cs
+++ SampleJetBA/View/RootView.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Ninject;
+﻿using Microsoft.Win32;
+using Ninject;
 using PanelSW.Installer.JetBA.ViewModel;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Interop;
 
@@ -8,7 +10,7 @@ namespace SampleJetBA.View
 {
     public partial class RootView : Window
     {
-        public RootView(ProgressViewModel prog, PopupViewModel popup, VariablesViewModel vars, NavigationViewModel nav, UtilViewModel util)
+        public RootView(ProgressViewModel prog, PopupViewModel popup, JetBundleVariables.BundleVariablesViewModel vars, NavigationViewModel nav, UtilViewModel util)
         {
             ProgressViewModel = prog;
             PopupViewModel = popup;
@@ -30,7 +32,7 @@ namespace SampleJetBA.View
 
         public ProgressViewModel ProgressViewModel { get; private set; }
         public PopupViewModel PopupViewModel { get; private set; }
-        public VariablesViewModel VariablesViewModel { get; private set; }
+        public JetBundleVariables.BundleVariablesViewModel VariablesViewModel { get; private set; }
         public NavigationViewModel NavigationViewModel { get; private set; }
         public UtilViewModel UtilViewModel { get; private set; }
 
@@ -42,6 +44,35 @@ namespace SampleJetBA.View
         private void Minimize_Click(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
+        }
+
+        private void ApplicationCommandsOpen_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            string prm = e.Parameter?.ToString();
+            if (string.IsNullOrEmpty(prm))
+            {
+                return;
+            }
+            int i = prm.IndexOf('|');
+            if ((i <= 0) || (i >= prm.Length))
+            {
+                return;
+            }
+
+            string varName = prm.Substring(0, i);
+            string browseType = prm.Substring(i + 1);
+            if (browseType.Equals("Directory"))
+            {
+                using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+                {
+                    fbd.Description = VariablesViewModel.WixBundleName.String;
+                    fbd.SelectedPath = VariablesViewModel[varName].String;
+                    if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        VariablesViewModel[varName].String = fbd.SelectedPath;
+                    }
+                }
+            }
         }
     }
 }
