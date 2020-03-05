ab1a047c97e931ada6fcca480334bcfe4bcc0cfa
Nir Bar <nir.bar@panel-sw.co.il>
2020-03-05 13:37:13 +0200

    Flash window while popup is open

--- SampleJetBA/View/RootView.xaml.cs
+++ SampleJetBA/View/RootView.xaml.cs
@@ -1,13 +1,56 @@
 ﻿using PanelSW.Installer.JetBA.Util;
 using PanelSW.Installer.JetBA.ViewModel;
+using System;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Input;
+using System.Windows.Interop;
 
 namespace SampleJetBA.View
 {
     public partial class RootView : Window
     {
+        #region P/Invoke
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool FlashWindowEx(ref FLASHWINFO pwfi);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct FLASHWINFO
+        {
+            public Int32 cbSize;
+            public IntPtr hwnd;
+            public FlashWindow dwFlags;
+            public UInt32 uCount;
+            public UInt32 dwTimeout;
+        }
+
+        [Flags]
+        public enum FlashWindow : uint
+        {
+            // Stop flashing. The system restores the window to its original state.
+            FLASHW_STOP = 0,
+
+            // Flash the window caption
+            FLASHW_CAPTION = 1,
+
+            // Flash the taskbar button.
+            FLASHW_TRAY = 2,
+
+            // Flash both the window caption and taskbar button.
+            FLASHW_ALL = 3,
+
+            // Flash continuously, until the FLASHW_STOP flag is set.
+            FLASHW_TIMER = 4,
+
+            // Flash continuously until the window comes to the foreground.
+            FLASHW_TIMERNOFG = 12
+        }
+
+        #endregion
+
         public RootView(ProgressViewModel prog, PopupViewModel popup, JetBundleVariables.BundleVariablesViewModel vars, NavigationViewModel nav, UtilViewModel util)
         {
             ProgressViewModel = prog;
@@ -67,6 +110,25 @@ namespace SampleJetBA.View
         private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = true;
+        }
+
+        private void popup_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            FLASHWINFO flash = new FLASHWINFO();
+            flash.cbSize = Marshal.SizeOf(flash.GetType());
+            flash.hwnd = new WindowInteropHelper(this).EnsureHandle();
+            flash.uCount = 0;
+            flash.dwTimeout = 0;
+            if (e.NewValue.Equals(true))
+            {
+                flash.dwFlags = FlashWindow.FLASHW_ALL | FlashWindow.FLASHW_TIMER;
+                FlashWindowEx(ref flash);
+            }
+            else
+            {
+                flash.dwFlags = FlashWindow.FLASHW_STOP;
+                FlashWindowEx(ref flash);
+            }
         }
     }
 }
