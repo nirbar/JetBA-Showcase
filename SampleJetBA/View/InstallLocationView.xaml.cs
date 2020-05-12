4521843b47ce7a592cf56ccd064fbd009651a8f7
Nir Bar <nir.bar@panel-sw.co.il>
2020-05-12 09:27:57 +0300

    Remove deprecated command

--- SampleJetBA/View/InstallLocationView.xaml.cs
+++ SampleJetBA/View/InstallLocationView.xaml.cs
@@ -1,18 +1,14 @@
-﻿using System.Windows.Controls;
-using PanelSW.Installer.JetBA.ViewModel;
-using System.Windows;
-using WinForms = System.Windows.Forms;
+﻿using PanelSW.Installer.JetBA.ViewModel;
+using System.Windows.Controls;
 
 namespace SampleJetBA.View
 {
     public partial class InstallLocationView : UserControl
     {
-        public InstallLocationView(JetBundleVariables.BundleVariablesViewModel vars, NavigationViewModel nav, ApplyViewModel apply, PopupViewModel popup)
+        public InstallLocationView(JetBundleVariables.BundleVariablesViewModel vars, NavigationViewModel nav)
         {
             NavigationViewModel = nav;
             VariablesViewModel = vars;
-	        ApplyViewModel = apply;
-            PopupViewModel = popup;
 
             DataContext = this;
             InitializeComponent();
@@ -20,22 +16,5 @@ namespace SampleJetBA.View
 
         public JetBundleVariables.BundleVariablesViewModel VariablesViewModel { get; private set; }
         public NavigationViewModel NavigationViewModel { get; private set; }
-        public ApplyViewModel ApplyViewModel { get; private set; }
-        public PopupViewModel PopupViewModel { get; private set; }
-
-		private void browseInstallFolder_Click(object sender, RoutedEventArgs e)
-        {
-            WinForms.FolderBrowserDialog fbd = new WinForms.FolderBrowserDialog();
-            fbd.ShowNewFolderButton = true;
-            if (!VariablesViewModel.INSTALL_FOLDER.IsNullOrEmpty)
-            {
-                fbd.SelectedPath = VariablesViewModel.INSTALL_FOLDER.String;
-            }
-
-            if (fbd.ShowDialog() == WinForms.DialogResult.OK)
-            {
-                VariablesViewModel.INSTALL_FOLDER.String = fbd.SelectedPath;
-            }
-        }
     }
 }
